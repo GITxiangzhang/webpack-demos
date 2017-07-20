1d12295b441e1f24316cd88f0068ee1fe153c124
GITxiangzhang <1213086100@qq.com>
2017-07-20 17:20:28 +0800

    webpack

--- demo03/main.jsx
+++ demo03/main.jsx
@@ -1,7 +1,8 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+const React= require('react');
+const ReactDOM=require('react-dom');
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.querySelector('#wrapper')
+    <h1>hello world!!</h1>,
+    document.getElementById('wrapper')
+
 );
